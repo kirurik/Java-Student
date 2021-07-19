f0d43bacb9604b4a9183ab2354c043adcaa972dc
Uri <81261810+kirurik@users.noreply.github.com>
2021-07-19 02:24:03 -0500

    Añade java ejemplo for

---
+++ forLoop.java
@@ -0,0 +1,11 @@
+// Java program to illustrate for loop.
+public class forLoop
+{
+    public static void main(String args[])
+    {
+        // for loop begins when x=0
+        // and runs till x <=4
+        for (int x = 0; x <= 4; x++)
+            System.out.println("Value of x:" + x);
+    }
+}
