2cbffa97e1ca86bd38c2f07f1d6a038d052b731f
Uri <81261810+kirurik@users.noreply.github.com>
2021-07-19 02:19:29 -0500

    Añade java ejemplo while

---
+++ whileLoop.java
@@ -0,0 +1,20 @@
+// Java program while loop example
+public class whileLoop
+{
+    public static void main(String args[])
+    {
+        
+        //Initialize constant
+        int x = 0;
+  
+        // Exit when x becomes greater than 4
+        while (x <= 10)
+        {
+            System.out.println("Value of x:" + x);
+  
+            // Increment the value of x for
+            // next iteration
+            x++;
+        }
+    }
+}
